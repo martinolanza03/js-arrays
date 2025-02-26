fb162fa9d25bc85c1fb7dba6d5c54c84be9fe6e7
martinolanza03 <lanzamartino3@gmail.com>
2025-02-26 16:18:16 +0100

    fix: fix exercise

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -51,6 +51,6 @@ for (let i = 0; i <= teachers.length; i++) {
 // e salva il risultato nella variabile isTeachersEmpty
 
 if (teachers.length > 0) {
-  const isTeachersEmpty = false;
+  const isTeachersEmpty = teachers;
   console.log(isTeachersEmpty)
 }
